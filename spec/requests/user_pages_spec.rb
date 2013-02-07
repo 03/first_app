de9d570005a32d7d73431107746e2f0ccbf67c76
Luke Feng <feng.oz@hotmail.com>
2013-02-07 17:01:08 +1100

    added validators

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,4 +9,10 @@ describe "User Pages" do
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
   end
+
+  #describe "pending page" do
+  #  before { visit signup_path }
+  #  pending "add some examples to (or delete) #{__FILE__}"
+  #end
+
 end

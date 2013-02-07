ebad31d835b6823f12979631108b12419e706df8
Luke <feng.oz@hotmail.com>
2013-02-07 22:46:22 +1100

    user authentication part1

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,13 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name
   has_many :microposts
 
+  before_save { |user| user.email = email.downcase }
+
   #validates(:name, presence: true)
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
 end

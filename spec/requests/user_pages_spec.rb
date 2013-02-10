48d323ff7784ffed2a6505642ea4e1996a8ce114
Luke <feng.oz@hotmail.com>
2013-02-10 21:48:32 +1100

    added signup pages

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,6 +10,15 @@ describe "User Pages" do
     it { should have_selector('title', text: full_title('Sign up')) }
   end
 
+  describe "profile page" do
+    # Code to make a user variable
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+
   #describe "pending page" do
   #  before { visit signup_path }
   #  pending "add some examples to (or delete) #{__FILE__}"

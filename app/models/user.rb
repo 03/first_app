9b283caa3b6a6527497da0ad7158db6c00a9e956
Luke <feng.oz@hotmail.com>
2013-02-14 21:24:48 +1100

    refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
   #validates(:name, presence: true)
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
